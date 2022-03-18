1336f0fe9b626d045b9738099c250be50373b5db
Neotrash788 <82312125+Neotrash788@users.noreply.github.com>
2022-03-18 21:25:08 +0000

    Update ai_transposition_table.c

--- ai_transposition_table.c
+++ ai_transposition_table.c
@@ -7,12 +7,14 @@
 #include <stdint.h>
 #include <limits.h>
 #include "uthash.h"
+
 #define HEIGHT 6
 #define WIDTH 7
 #define BOARDLEN 64
 #define ANSI_COLOR_RED     "\x1b[31m" 
 #define ANSI_COLOR_YELLOW  "\x1b[33m" 
 #define ANSI_COLOR_RESET   "\x1b[0m" 
+
 unsigned long long count = 0;
 
 struct boardState {
